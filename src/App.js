4a63046ccc86a7c2189f1f05f1ed2293f78421a7
Ep1xo <novikovworking@gmail.com>
2025-04-15 19:10:37 +0300

    Added Photos and completed header part of the site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import {React, useState} from 'react';
+import ContactWindow from "./ContactWindow";
 
 function App() {
+  const [showContactWindow, setShowContactWindow] = useState(false);
+
+  const openContactWindow = () => setShowContactWindow(true);
+  const closeContactWindow = () => setShowContactWindow(false);
+
   return (
     <>
-      
+      <header>
+        <div className="navigation">
+          <button>Головна</button>
+          <button>Портфоліо</button>
+          <button>Послуги</button>
+          <button>FAQ</button>
+        </div>
+        <div className="contacts">
+          <button onClick={openContactWindow}>Контакти</button>
+          <a href="https://www.instagram.com/katerinn.video?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D" target= "_blank"><img src="/icons/instagram.svg"/></a>
+        </div>
+
+        <ContactWindow show={showContactWindow} onClose={closeContactWindow} />
+      </header>
     </>
   );
 }
